7eb87a33445836863772871910b416d2bc2c5a0b
Georg Brandl <georg@python.org>
2020-11-26 08:13:20 +0100

    Prepare for 2020 edition.

---
+++ src/bin/day01.rs
@@ -0,0 +1,11 @@
+// use advtools::itertools::Itertools;
+// use advtools::input::input_string;
+use advtools::input::iter_input;
+
+fn main() {
+    // let code = input_string();
+
+    advtools::verify("1", x, y);
+
+    advtools::verify("2", x, y);
+}
