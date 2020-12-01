90cdc8716f4929bf200dc3f1514328084fab5705
Georg Brandl <georg@python.org>
2020-12-01 09:13:09 +0100

    Day 1.

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,11 +1,22 @@
-// use advtools::itertools::Itertools;
-// use advtools::input::input_string;
+use advtools::itertools::Itertools;
 use advtools::input::iter_input;
 
 fn main() {
-    // let code = input_string();
+    let items: Vec<i32> = iter_input().sorted().collect();
 
-    advtools::verify("1", x, y);
+    for comb in items.iter().combinations(2) {
+        if comb[0] + comb[1] == 2020 {
+            let product = comb[0] * comb[1];
+            advtools::verify("Product of 2", product, 41979);
+            break;
+        }
+    }
 
-    advtools::verify("2", x, y);
+    for comb in items.iter().combinations(3) {
+        if comb[0] + comb[1] + comb[2] == 2020 {
+            let product = comb[0] * comb[1] * comb[2];
+            advtools::verify("Product of 3", product, 193416912);
+            break;
+        }
+    }
 }
