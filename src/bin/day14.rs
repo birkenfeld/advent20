77f66f079021787bbdc6acd68beb8e4fda500366
Georg Brandl <georg@python.org>
2020-12-14 10:49:49 +0100

    Day 14.

---
+++ src/bin/day14.rs
@@ -0,0 +1,59 @@
+use advtools::prelude::*;
+use advtools::input::{iter_lines, to_u64};
+
+fn process<M, X, FM, FA>(mut process_mask: FM, mut process_assign: FA)
+where FM: FnMut(u64, u64, u64) -> M,
+      FA: FnMut(u64, u64, &M) -> X
+{
+    let mut mask_state = None;
+    for line in iter_lines() {
+        if line.starts_with("mask =") {
+            let (mut zero, mut one) = (0, 0);
+            for (i, ch) in line[7..].chars().enumerate() {
+                let bit = 35-i;
+                match ch {
+                    '0' => zero |= 1 << bit,
+                    '1' => one  |= 1 << bit,
+                    _ => ()
+                }
+            }
+            mask_state = Some(process_mask(zero, one, !zero & !one));
+        } else {
+            let mut parts = line.split(|ch| ch == '[' || ch == ']');
+            let addr = to_u64(parts.nth(1).unwrap());
+            let val = to_u64(&parts.next().unwrap()[3..]);
+            process_assign(addr, val, mask_state.as_ref().unwrap());
+        }
+    }
+}
+
+fn all_masks(addr: u64, bits: u64, bit: usize, masks: &mut Vec<u64>) {
+    if bit == 36 {
+        masks.push(addr);
+    } else {
+        if bits & (1 << bit) != 0 {
+            all_masks(addr | (1 << bit), bits, bit+1, masks);
+        }
+        all_masks(addr, bits, bit+1, masks);
+    }
+}
+
+fn main() {
+    let mut mem = HashMap::new();
+    process(
+        |zero, one, _| (!zero, one),
+        |addr, val, &(and, or)| mem.insert(addr, (val & and) | or)
+    );
+
+    advtools::verify("Sum of memory", mem.values().sum::<u64>(), 4886706177792u64);
+
+    mem.clear();
+    process(
+        |_, one, x| { let mut ors = vec![]; all_masks(0, x, 0, &mut ors); (!x, one, ors) },
+        |addr, val, (and, or, ors)| for or1 in ors.iter() {
+            mem.insert((addr & and) | or | or1, val);
+        }
+    );
+
+    advtools::verify("Sum of memory", mem.values().sum::<u64>(), 3348493585827u64);
+}
