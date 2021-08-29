c8f32a8805686e0e3f4ee55a2780992f363fcff9
Georg Brandl <georg@python.org>
2021-08-29 10:18:07 +0200

    Day 17: use single contiguous Vec.

--- src/bin/day17.rs
+++ src/bin/day17.rs
@@ -1,57 +1,51 @@
 use advtools::prelude::*;
 use advtools::input::iter_lines;
 use advtools::itertools::iproduct;
+use std::ops::Range;
 
-const NXY: usize = 21;
-const NZW: usize = 13;
+const N: isize = 18;
 
-fn process(grid: &mut Vec<Vec<Vec<Vec<bool>>>>, diffs: &[(isize, isize, isize, isize)]) -> usize {
-    for _ in 0..6 {
+fn index(mut w: isize, mut z: isize, mut y: isize, mut x: isize) -> Option<usize> {
+    w += N/2; z += N/2; y += N/2; x += N/2;
+    (w >= 0 && w < N && z >= 0 && z < N && y >= 0 && y <= N && x >= 0 && x <= N)
+        .then(|| (w * N*N*N + z * N*N + y * N + x) as usize)
+}
+
+fn process(grid: &mut [bool], diffs: &[(isize, isize, isize, isize)], wrange: Option<Range<isize>>) -> usize {
+    for round in 0..6 {
         let mut flips = vec![];
-        for (w, dim) in grid.iter().enumerate() {
-            for (z, plane) in dim.iter().enumerate() {
-                for (y, row) in plane.iter().enumerate() {
-                    for (x, cell) in row.iter().enumerate() {
-                        let mut count = 0;
-                        for &(dw, dz, dy, dx) in diffs {
-                            let nw = (w as isize) + dw;
-                            if nw < 0 || nw >= grid.len() as isize { continue; }
-                            let nz = (z as isize) + dz;
-                            if nz < 0 || nz >= NZW as isize { continue; }
-                            let ny = (y as isize) + dy;
-                            if ny < 0 || ny >= NXY as isize { continue; }
-                            let nx = (x as isize) + dx;
-                            if nx < 0 || nx >= NXY as isize { continue; }
-                            count += grid[nw as usize][nz as usize][ny as usize][nx as usize] as u32;
-                        }
-                        if *cell {
-                            if count < 2 || count > 3 { flips.push((w, z, y, x)); }
-                        } else {
-                            if count == 3 { flips.push((w, z, y, x)); }
+        for w in wrange.clone().unwrap_or(-round-2..round+2) {
+            for z in -N/2..N/2 {
+                for y in -N/2..N/2 {
+                    for x in -N/2..N/2 {
+                        let nbs = diffs.iter()
+                                       .filter_map(|&(dw, dz, dy, dx)| index(w+dw, z+dz, y+dy, x+dx))
+                                       .filter(|&j| grid[j]).count();
+                        let i = index(w, z, y, x).unwrap();
+                        if (grid[i] && !(2..=3).contains(&nbs)) || (!grid[i] && nbs == 3) {
+                            flips.push(i);
                         }
                     }
                 }
             }
         }
-        for (w, z, y, x) in flips {
-            grid[w][z][y][x] = !grid[w][z][y][x];
-        }
+        flips.into_iter().for_each(|i| grid[i] = !grid[i]);
     }
-    grid.iter().flatten().flatten().flatten().filter(|x| **x).count()
+    grid.iter().filter(|&&x| x).count()
 }
 
 fn main() {
-    let mut grid = vec![vec![vec![vec![false; NXY]; NXY]; NZW]; NZW];
+    let mut grid = vec![false; (N*N*N*N) as usize];
     for (y, line) in iter_lines().enumerate() {
         for (x, ch) in line.chars().enumerate() {
-            if ch == '#' { grid[NZW/2][NZW/2][y+NXY/2-4][x+NXY/2-4] = true; }
+            if ch == '#' { grid[index(0, 0, y as isize-4, x as isize-4).unwrap()] = true; }
         }
     }
+    let mut grid3 = grid.clone();
+
     let mut diffs = iproduct!(-1..=1, -1..=1, -1..=1, -1..=1).collect_vec();
     diffs.retain(|&x| x != (0, 0, 0, 0));
 
-    let mut grid3 = vec![grid[NZW/2].clone()];
-
-    advtools::verify("3-d grid", process(&mut grid3, &diffs), 301);
-    advtools::verify("4-d grid", process(&mut grid, &diffs), 2424);
+    advtools::verify("3-d grid", process(&mut grid3, &diffs, Some(0..1)), 301);
+    advtools::verify("4-d grid", process(&mut grid, &diffs, None), 2424);
 }
