232d9d51cd8e6b24753c6f9f7377287ddd1cfa57
Georg Brandl <georg@python.org>
2021-09-05 07:17:09 +0200

    Day 3: use Grid.

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,20 +1,18 @@
 use advtools::input::iter_lines;
+use advtools::grid::Grid;
+
+const DIRECTIONS: &[(usize, usize)] = &[(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
 
 fn main() {
-    let directions = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)];
+    let grid = Grid::new(iter_lines().map(|line| line.chars().collect()));
 
-    let grid: Vec<Vec<char>> = iter_lines()
-        .map(|line| line.chars().collect())
-        .collect();
-    let nx = grid[0].len();
-
-    let count_trees = |&(right, down)| (0..grid.len()).step_by(down)
-        .zip((0..).step_by(right))
-        .filter(|&(y, x)| grid[y][x % nx] == '#')
+    let count_trees = |&(right, down)| (0..).step_by(right)
+        .zip((0..grid.height()).step_by(down))
+        .filter(|&(x, y)| grid[(x % grid.width(), y)] == '#')
         .count();
 
     advtools::verify("Trees with 1/3", count_trees(&(3, 1)), 257);
 
-    let prod: usize = directions.iter().map(count_trees).product();
+    let prod: usize = DIRECTIONS.iter().map(count_trees).product();
     advtools::verify("Product of trees", prod, 1744787392);
 }
